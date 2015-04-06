9cedade61538f92600fb0a0de5f53c9ef580a3be
Kingloo <k1ngloo@hotmail.com>
2015-04-06 15:39:42 +0200

    test

--- GB-Live/Globals.cs
+++ GB-Live/Globals.cs
@@ -4,7 +4,7 @@ namespace GB_Live
 {
     public static class Globals
     {
-        public static Uri gbHome = new Uri("http://www.giantbomb.com/");
+        public static Uri gbHome = new Uri("http://www.giantbomb.com/"); // comment
         public static Uri gbChat = new Uri("http://www.giantbomb.com/chat");
     }
 }
