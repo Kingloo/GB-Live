0c5b177f5e6d0a72c5b2661d6f2916d0626c66ff
Kingloo <k1ngloo@hotmail.com>
2019-10-20 16:13:20 +0200

    Initial structure

---
+++ test/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace GBLive.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
