7a5eca20639b7ec66679a7d49fd3c7455eeddf19
Kingloo <k1ngloo@hotmail.com>
2019-12-08 14:57:12 +0100

    Nullable reference types

--- src/Converters/BooleanConverterBase.cs
+++ src/Converters/BooleanConverterBase.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics.CodeAnalysis;
 using System.Globalization;
 using System.Windows;
 using System.Windows.Data;
@@ -23,18 +24,19 @@ namespace GBLive.Converters
             "True",
             typeof(T),
             typeof(BooleanConverterBase<T>),
-            new PropertyMetadata(default(T)));
+            new PropertyMetadata(null));
 
         public static readonly DependencyProperty FalseProperty = DependencyProperty.Register(
             "False",
             typeof(T),
             typeof(BooleanConverterBase<T>),
-            new PropertyMetadata(default(T)));
+            new PropertyMetadata(null));
 
+        [return: MaybeNull]
         public virtual object Convert(object value, Type targetType, object parameter, CultureInfo culture)
             => (bool)value ? True : False;
 
         public virtual object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
-            => default(T);
+            => throw new NotImplementedException($"converting from {value.GetType().ToString()} to {targetType.ToString()} is not implemented!");
     }
 }
