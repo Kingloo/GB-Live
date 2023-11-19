3da73f6a499f881915a1dcf177b6c032ded65785
Kingloo <k1ngloo@hotmail.com>
2023-11-19 16:07:40 +0100

    Update project files and publish script for .NET 8

--- publish-desktop-x64.ps1
+++ publish-desktop-x64.ps1
@@ -1,3 +1,3 @@
 dotnet restore
 dotnet build .\GBLive.sln -c Release --no-restore --nologo
-dotnet publish .\src\GBLive.csproj -c Release -r win10-x64 /p:PublishSingleFile=true --no-self-contained --no-restore
+dotnet publish .\src\GBLive.csproj -c Release -r win-x64 /p:PublishSingleFile=true --no-self-contained --no-restore
