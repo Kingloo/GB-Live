51b84e7462607808de44b37f4d4722f1d0ac7671
Kingloo <k1ngloo@hotmail.com>
2024-11-14 22:54:20 +0100

    Add DisallowNull to URI props

--- src/Options/GiantBombOptions.cs
+++ src/Options/GiantBombOptions.cs
@@ -1,12 +1,17 @@
 using System;
+using System.Diagnostics.CodeAnalysis;
 
 namespace GBLive.Options
 {
 	public class GiantBombOptions
 	{
+		[DisallowNull]
 		public Uri? HomePage { get; init; }
+		[DisallowNull]
 		public Uri? ChatPage { get; init; }
+		[DisallowNull]
 		public Uri? UpcomingJsonUri { get; init; }
+		[DisallowNull]
 		public Uri? FallbackImageUri { get; init; }
 	}
 }
